baa499b06187ed70a25a7205fb7774bdc8ae1e03
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 11:43:10 +0300

    Added some markup to the component

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav className='h-[5rem] w-full border-b-2 flex items-center px-5 uppercase text-2xl justify-between'>NavBar</nav>
+	return (
+		<nav className='h-[5rem] w-full border-b-2 flex items-center px-5 uppercase text-2xl justify-between'>
+			<h1>Full Calendar</h1>
+            
+		</nav>
+	)
 }
 
 export default NavBar
