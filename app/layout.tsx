85274ded38ec6ca7ec899209db99fb9ce2156c4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 11:33:15 +0300

    Added the redux provider to the layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ReduxProvider from '@/Providers/ReduxProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Full Calendar Example',
-  description: 'A Simple Full Calendar App',
+	title: 'Full Calendar Example',
+	description: 'A Simple Full Calendar App'
 }
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<ReduxProvider>
+				<body className={inter.className}>{children}</body>
+			</ReduxProvider>
+		</html>
+	)
 }
