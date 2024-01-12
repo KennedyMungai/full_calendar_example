8575282aa425f48f7f9066e69b0e9b131d80312a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 12:05:50 +0300

    Rendered out the showDate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import ShowDate from '@/components/ShowingComponent/ShowDate'
+
 export default function Home() {
-  return (
-    <main></main>
-  )
+	return (
+		<main className='h-[30rem] flex items-center justify-center'>
+			<ShowDate />
+		</main>
+	)
 }
