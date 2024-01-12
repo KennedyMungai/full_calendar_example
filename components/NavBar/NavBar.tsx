20cd4fa955599496e09e72120f7c4f6bc6d3c803
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 11:52:25 +0300

    Added a handleClick handler function for when individual dates are clicked

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,13 +1,33 @@
 'use client'
-import React from 'react'
+import DayGridPlugin from '@fullcalendar/daygrid'
+import InteractionPlugin from '@fullcalendar/interaction'
+import FullCalendar from '@fullcalendar/react'
+import { Button } from '../ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
+    const handleDateClick = (arg:any) => {
+        console.log(arg.dateStr)
+    }
+
 	return (
 		<nav className='h-[5rem] w-full border-b-2 flex items-center px-5 uppercase text-2xl justify-between'>
 			<h1>Full Calendar</h1>
-            
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button variant={'outline'}>Get Date</Button>
+				</PopoverTrigger>
+				<PopoverContent className='w-full'>
+					<div className=''>
+						<FullCalendar
+                            plugins={[DayGridPlugin, InteractionPlugin]}
+                            dateClick={handleDateClick}
+                        />
+					</div>
+				</PopoverContent>
+			</Popover>
 		</nav>
 	)
 }
