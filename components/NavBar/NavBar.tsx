a3c7982ada35a0e6e7a346caebb6ce3d25ee3243
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 11:38:02 +0300

    Added some styles for the NavBar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav className='h-[5rem] w-full'>NavBar</nav>
+	return <nav className='h-[5rem] w-full border-b-2 flex items-center px-5 uppercase text-2xl'>NavBar</nav>
 }
 
 export default NavBar
