60fbeb70637c89f6da7d0967ffd67edef620f169
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 11:38:31 +0300

    Added the NavBar component to the layout of the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ReduxProvider from '@/Providers/ReduxProvider'
+import NavBar from '@/components/NavBar/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,12 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<ReduxProvider>
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<NavBar />
+					{children}
+				</body>
 			</ReduxProvider>
 		</html>
 	)
